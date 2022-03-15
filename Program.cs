3971b3153d098efa05cbc5dbab055d80349a874a
Drew Miller <millerdrew@gmail.com>
2022-03-15 01:10:09 -0400

    fix: move redis into razor component

--- Program.cs
+++ Program.cs
@@ -3,19 +3,13 @@ using Tailwind;
 using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
+var multiplexer = ConnectionMultiplexer.Connect("localhost");
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-
-// Add Redis
-ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
-IDatabase db = redis.GetDatabase();
-string value1 = "abcdefg";
-db.StringSet("mykey", value1);
-string value2 = db.StringGet("mykey");
-Console.WriteLine(value2); // writes: "abcdefg"
+builder.Services.AddSingleton<IConnectionMultiplexer>(multiplexer);
 
 var app = builder.Build();
 
